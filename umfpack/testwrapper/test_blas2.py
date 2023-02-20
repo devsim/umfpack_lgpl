f50fc413c449c4c3e48ac60dc2d5f919e3d25df2
Juan E. Sanchez <juan@tcad.com>
2023-02-19 18:01:03 -0600

    work on interface

--- umfpack/testwrapper/test_blas2.py
+++ umfpack/testwrapper/test_blas2.py
@@ -20,38 +20,12 @@ b = array.array('d', (8., 45., -3., 3., 19.))
 x = array.array('d', [0.0]*n)
 r = array.array('d', [0.0]*n)
 
-#/* -------------------------------------------------------------------------- */
-#/* resid: compute the residual, r = Ax-b or r = A'x=b and return maxnorm (r) */
-#/* -------------------------------------------------------------------------- */
-def resid ( transpose, Ap, Ai, Ax):
-  n = len(b)
-  for i in range(n): 
-    r[i] = -b[i]
-  if (transpose):
-    for j in range(n): 
-      for p in range(Ap[j], Ap[j+1]):
-        i = Ai [p]
-        r [j] += Ax [p] * x [i]
-  else:
-    for j in range(n):
-      for p in range(Ap[j], Ap[j+1]):
-        i = Ai [p]
-        r [i] += Ax [p] * x [j]
-  norm = 0.0
-  print(len(r))
-  for i in range(n):
-    if abs(r[i]) > norm:
-      norm = abs(r[i])
-  return norm
-
-
 #
 #    /* ---------------------------------------------------------------------- */
 #    /* initializations */
 #    /* ---------------------------------------------------------------------- */
 #
 umfclass = umf.umf_control(dll)
-
 umfclass.tic()
 
 umfclass.set_defaults()
@@ -67,19 +41,20 @@ umfclass.print_vector(b, 'b')
 #    /* print the triplet form of the matrix */
 
 print ("\nA: ")
-umfclass.print_triplet(Arow, Acol, Aval)
+triplet_matrix = umf.di_triplet(umfclass, Arow, Acol, Aval)
+umfclass.print_triplet(triplet_matrix)
 
 #    /* convert to column form */
-[Ap, Ai, Ax] = umfclass.triplet_to_col(Arow, Acol, Aval)
+matrix = umfclass.triplet_to_col(triplet_matrix)
 
 #    /* print the column-form of A */
-umfclass.print_matrix(Ap, Ai, Ax)
+umfclass.print_matrix(matrix)
 
 #    /* ---------------------------------------------------------------------- */
 #    /* symbolic factorization */
 #    /* ---------------------------------------------------------------------- */
 #
-Symbolic = umfclass.symbolic(Ap, Ai, Ax)
+Symbolic = umfclass.symbolic(matrix)
 #
 #
 #    /* print the symbolic factorization */
@@ -91,7 +66,7 @@ umfclass.print_symbolic(Symbolic)
 #    /* numeric factorization */
 #    /* ---------------------------------------------------------------------- */
 #
-Numeric = umfclass.numeric(Ap, Ai, Ax, Symbolic)
+Numeric = umfclass.numeric(matrix, Symbolic)
 
 
 #    /* print the numeric factorization */
@@ -102,13 +77,13 @@ umfclass.print_numeric(Numeric)
 #    /* solve Ax=b */
 #    /* ---------------------------------------------------------------------- */
 #
-status = umfclass.solve(Ap, Ai, Ax, x, b, Numeric)
+status = umfclass.solve(matrix, x, b, Numeric)
 umfclass.print_info()
 umfclass.print_status()
 
 print("\nx (solution of Ax=b): ")
 umfclass.print_vector(x, 'x')
-rnorm = resid(False, Ap, Ai, Ax)
+rnorm = umf.resid(transpose=False, matrix=matrix, x=x, r=r, b=b)
 print ("maxnorm of residual: %g\n\n" % rnorm)
 
 #   /* ---------------------------------------------------------------------- */
@@ -118,7 +93,5 @@ print ("maxnorm of residual: %g\n\n" % rnorm)
 umfclass.determinant(x, r, Numeric)
 umfclass.print_determinant(x, r)
 
-#dll.umfpack_di_free_symbolic (byref(Symbolic))
-#dll.umfpack_di_free_numeric (byref(Numeric))
 umfclass.toc()
-#
+
--- umfpack/testwrapper/umfpack_loader.py
+++ umfpack/testwrapper/umfpack_loader.py
@@ -151,28 +151,77 @@ def set_python_print_callback(dll):
 def get_info():
     return (c_int * uml.UMFPACK_INFO)();
 
+dll = None
+
+
+class di_symbolic:
+    def __init__(self, uc):
+        self.Symbolic = c_void_p()
+        self.umf_control = uc
+
+    def __del__(self):
+        dll.umfpack_di_free_symbolic (byref(self.Symbolic))
+
+class di_numeric:
+    def __init__(self, uc):
+        self.Numeric = c_void_p()
+        self.umf_control = uc
+
+    def __del__(self):
+        dll.umfpack_di_free_numeric (byref(self.Numeric))
+
+class di_triplet:
+    def __init__(self, uc, Arow, Acol, Aval):
+        self.umf_control = uc
+        self.n = max(Arow) + 1
+        self.nz = len(Arow)
+        self.Arow = Arow
+        self.Acol = Acol
+        self.Aval = Aval
+        self.Ar = c_void_p(Arow.buffer_info()[0])
+        self.Ac = c_void_p(Acol.buffer_info()[0])
+        self.Av = c_void_p(Aval.buffer_info()[0])
+
+    def __del__(self):
+        pass
+
+class di_matrix:
+    def __init__(self, uc, Ap, Ai, Ax):
+        self.umf_control = uc
+        self.n = len(Ap)-1
+        self.Ap = Ap
+        self.Ai = Ai
+        self.Ax = Ax
+        self.AP = c_void_p(Ap.buffer_info()[0])
+        self.AI = c_void_p(Ai.buffer_info()[0])
+        self.AX = c_void_p(Ax.buffer_info()[0])
+
+    def __del__(self):
+        pass
+
 class umf_control:
-    def __init__(self, dll):
+    def __init__(self, dllp):
+        global dll
         self.timer = None
-        self.dll = dll
+        dll = dllp
         self.Control = None
         self.Info = None
 
     def __del__(self):
         # having a destructor is preventing segmentation fault
-        self.dll = None
+        dll = None
 
     def tic(self):
         self.timer = (c_double * 2)()
-        self.dll.umfpack_tic(byref(self.timer))
+        dll.umfpack_tic(byref(self.timer))
 
     def toc(self):
-        self.dll.umfpack_toc (self.timer)
+        dll.umfpack_toc (self.timer)
         print ("\numfpack_di_demo complete.\nTotal time: %5.2f seconds (CPU time), %5.2f seconds (wallclock time)\n" % ( self.timer [1], self.timer [0]))
 
     def set_defaults(self):
         self.Control = (c_double * UMFPACK_CONTROL)()
-        self.dll.umfpack_di_defaults(byref(self.Control))
+        dll.umfpack_di_defaults(byref(self.Control))
         self.Info = (c_int * UMFPACK_INFO)()
 
     def init_verbose(self):
@@ -180,64 +229,51 @@ class umf_control:
         #    /* (otherwise, nothing will print) */
         self.Control [UMFPACK_PRL] = 6
         #    /* print the license agreement */
-        self.dll.umfpack_di_report_status (self.Control, UMFPACK_OK)
+        dll.umfpack_di_report_status (self.Control, UMFPACK_OK)
         self.Control [UMFPACK_PRL] = 5
         #
         #    /* print the control parameters */
-        self.dll.umfpack_di_report_control(self.Control)
+        dll.umfpack_di_report_control(self.Control)
 
     def print_vector(self, b, label):
         #    /* print the right-hand-side */
         print ("\n%s: " % (label,), flush=True, end="")
-        self.dll.umfpack_di_report_vector.argtypes = [c_int, c_void_p, c_void_p]
-        self.dll.umfpack_di_report_vector.restype = None
-        self.dll.umfpack_di_report_vector (len(b), b.buffer_info()[0], self.Control)
+        dll.umfpack_di_report_vector.argtypes = [c_int, c_void_p, c_void_p]
+        dll.umfpack_di_report_vector.restype = None
+        dll.umfpack_di_report_vector (len(b), b.buffer_info()[0], self.Control)
 
-    def print_triplet(self, Arow, Acol, Aval):
+    def print_triplet(self, tm):
         print ("\nA: ")
-        n = max(Arow) + 1
-        nz = len(Arow)
         #print(n)
         #print(nz)
-        Ar = c_void_p(Arow.buffer_info()[0])
-        Ac = c_void_p(Acol.buffer_info()[0])
-        Av = c_void_p(Aval.buffer_info()[0])
-        self.dll.umfpack_di_report_triplet(n, n, nz, Ar, Ac, Av, self.Control)
+        dll.umfpack_di_report_triplet(tm.n, tm.n, tm.nz, tm.Ar, tm.Ac, tm.Av, self.Control)
 
-    def triplet_to_col(self, Arow, Acol, Aval):
-        n = max(Arow) + 1
-        nz = len(Arow)
+    def triplet_to_col(self, tm):
+        n = tm.n
+        nz = tm.nz
         NULL = (c_void_p)()
         nz1 = max(nz,1) #; /* ensure arrays are not of size zero. */
         Ap = array.array('i', [0] * (n+1))
         Ai = array.array('i', [0] * (nz1))
         Ax = array.array('d', [0] * (nz1))
-        Ar = c_void_p(Arow.buffer_info()[0])
-        Ac = c_void_p(Acol.buffer_info()[0])
-        Av = c_void_p(Aval.buffer_info()[0])
-        AP = c_void_p(Ap.buffer_info()[0])
-        AI = c_void_p(Ai.buffer_info()[0])
-        AX = c_void_p(Ax.buffer_info()[0])
-        self.status = self.dll.umfpack_di_triplet_to_col (n, n, nz, Ar, Ac, Av, AP, AI, AX, NULL)
-        return Ap, Ai, Ax
+        matrix = di_matrix(self, Ap, Ai, Ax)
+        self.status = dll.umfpack_di_triplet_to_col (n, n, nz, tm.Ar, tm.Ac, tm.Av, matrix.AP, matrix.AI, matrix.AX, NULL)
 
         if self.status < 0:
-            self.dll.umfpack_di_report_status (self.Control, self.status)
+            dll.umfpack_di_report_status (self.Control, self.status)
             raise RuntimeError("umfpack_di_triplet_to_col failed")
 
-    def print_matrix(self, Ap, Ai, Ax):
+        return matrix
+
+    def print_matrix(self, matrix):
         print("\nA: ")
-        n = len(Ap)-1
-        AP = c_void_p(Ap.buffer_info()[0])
-        AI = c_void_p(Ai.buffer_info()[0])
-        AX = c_void_p(Ax.buffer_info()[0])
-        self.dll.umfpack_di_report_matrix (n, n, AP, AI, AX, 1, self.Control)
+        dll.umfpack_di_report_matrix (matrix.n, matrix.n, matrix.AP, matrix.AI, matrix.AX, 1, self.Control)
 
     def print_info(self):
-        self.dll.umfpack_di_report_info (self.Control, self.Info)
+        dll.umfpack_di_report_info (self.Control, self.Info)
 
     def print_status(self):
-        self.dll.umfpack_di_report_status (self.Control, self.status)
+        dll.umfpack_di_report_status (self.Control, self.status)
 
     def error_on_result(self, status, msg):
         if status < 0:
@@ -245,53 +281,69 @@ class umf_control:
             self.print_status (status)
             raise RuntimeError("%s failed" % (msg,))
 
-    def symbolic(self, Ap, Ai, Ax):
-        n = len(Ap)-1
-        Symbolic = c_void_p()
-        AP = c_void_p(Ap.buffer_info()[0])
-        AI = c_void_p(Ai.buffer_info()[0])
-        AX = c_void_p(Ax.buffer_info()[0])
-        status = self.dll.umfpack_di_symbolic (n, n, AP, AI, AX, byref(Symbolic), self.Control, self.Info)
+    def symbolic(self, matrix):
+        Symbolic = di_symbolic(self)
+        status = dll.umfpack_di_symbolic (matrix.n, matrix.n, matrix.AP, matrix.AI, matrix.AX, byref(Symbolic.Symbolic), self.Control, self.Info)
         self.error_on_result(status, "umfpack_di_symbolic")
         return Symbolic
 
     def print_symbolic(self, Symbolic):
         print("\nSymbolic factorization of A: ")
-        self.dll.umfpack_di_report_symbolic(Symbolic, self.Control)
+        dll.umfpack_di_report_symbolic(Symbolic.Symbolic, self.Control)
 
 
-    def numeric(self, Ap, Ai, Ax, Symbolic):
-        Numeric = c_void_p()
-        self.dll.umfpack_di_free_numeric (byref(Numeric))
-        AP = c_void_p(Ap.buffer_info()[0])
-        AI = c_void_p(Ai.buffer_info()[0])
-        AX = c_void_p(Ax.buffer_info()[0])
-        status = self.dll.umfpack_di_numeric (AP, AI, AX, Symbolic, byref(Numeric), self.Control, self.Info)
+    def numeric(self, matrix, Symbolic):
+        Numeric = di_numeric(self)
+        status = dll.umfpack_di_numeric (matrix.AP, matrix.AI, matrix.AX, Symbolic.Symbolic, byref(Numeric.Numeric), self.Control, self.Info)
         self.error_on_result(status, "umfpack_di_numeric")
         return Numeric
 
     def print_numeric(self, Numeric):
         print ("\nNumeric factorization of A: ")
-        self.dll.umfpack_di_report_numeric (Numeric, self.Control)
+        dll.umfpack_di_report_numeric (Numeric.Numeric, self.Control)
 
-    def solve(self, Ap, Ai, Ax, x, b, Numeric):
+    def solve(self, matrix, x, b, Numeric):
         X = c_void_p(x.buffer_info()[0])
         B = c_void_p(b.buffer_info()[0])
-        AP = c_void_p(Ap.buffer_info()[0])
-        AI = c_void_p(Ai.buffer_info()[0])
-        AX = c_void_p(Ax.buffer_info()[0])
-        status = self.dll.umfpack_di_solve (UMFPACK_A, AP, AI, AX, X, B, Numeric, self.Control, self.Info)
+        status = dll.umfpack_di_solve (UMFPACK_A, matrix.AP, matrix.AI, matrix.AX, X, B, Numeric.Numeric, self.Control, self.Info)
         self.error_on_result(status, "umfpack_di_solve")
         return b
 
     def determinant(self, x, r, Numeric):
         X = c_void_p(x.buffer_info()[0])
         R = c_void_p(r.buffer_info()[0])
-        status = self.dll.umfpack_di_get_determinant (X, R, Numeric, self.Info)
+        status = dll.umfpack_di_get_determinant (X, R, Numeric.Numeric, self.Info)
         self.error_on_result(status, "umfpack_di_get_determinant")
         return status
 
     def print_determinant(self, x, r):
         print("determinant: (%g" % x [0], end="")
         print(") * 10^(%g)\n"% r [0])
+
+#/* -------------------------------------------------------------------------- */
+#/* resid: compute the residual, r = Ax-b or r = A'x=b and return maxnorm (r) */
+#/* -------------------------------------------------------------------------- */
+def resid ( transpose, matrix, x, r, b):
+  Ap = matrix.Ap
+  Ai = matrix.Ai
+  Ax = matrix.Ax
+  n = len(b)
+  for i in range(n):
+    r[i] = -b[i]
+  if (transpose):
+    for j in range(n):
+      for p in range(Ap[j], Ap[j+1]):
+        i = Ai [p]
+        r [j] += Ax [p] * x [i]
+  else:
+    for j in range(n):
+      for p in range(Ap[j], Ap[j+1]):
+        i = Ai [p]
+        r [i] += Ax [p] * x [j]
+  norm = 0.0
+  print(len(r))
+  for i in range(n):
+    if abs(r[i]) > norm:
+      norm = abs(r[i])
+  return norm
 
