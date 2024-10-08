d98d5cc10d8da1dfb5aa8f31143fd4f7d26d98d2
Juan E. Sanchez <juan@tcad.com>
2024-10-08 14:20:55 -0500

    work on clangx64

--- build_appveyor.sh
+++ build_appveyor.sh
@@ -4,12 +4,13 @@ set -u
 
 GENERATOR="$1"
 AOPTION="$2"
-BUILDDIR="$3"
+TOOLSET="$3"
+BUILDDIR="$4"
 
 (\
 mkdir -p ${BUILDDIR} && \
 cd ${BUILDDIR} && \
-cmake -G "${GENERATOR}" -A "${AOPTION}" ../umfpack && \
+cmake -G "${GENERATOR}" -A "${AOPTION}" -T "${TOOLSET}" ../umfpack && \
 cmake --build . --config Release -- //m //nologo //verbosity:minimal \
 )
 
